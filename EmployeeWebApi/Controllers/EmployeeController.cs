049387f8b20c9b65b21b1196dfd10206b52b8c0e
Abent Kirill <abent.kirill@outlook.com>
2024-06-06 18:30:54 +0400

    Задокументировал контроллер

--- EmployeeWebApi/Controllers/EmployeeController.cs
+++ EmployeeWebApi/Controllers/EmployeeController.cs
@@ -1,12 +1,19 @@
 ﻿using EmployeeWebApi.Models;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeWebApi.Controllers;
 
 [ApiController]
+[Produces("application/json")]
 [Route("[controller]")]
 public sealed class EmployeeController(EmployeeRepository employeeRepository) : ControllerBase
 {
+    /// <summary>
+    /// Добавление сотрудника
+    /// </summary>
+    /// <param name="employee">Данные сотрудника</param>
+    /// <returns>Id добавленного сотрудника</returns>
     [HttpPost]
     public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
     {
@@ -14,6 +21,11 @@ public sealed class EmployeeController(EmployeeRepository employeeRepository) : 
         return Ok(new { Id = id });
     }
 
+    /// <summary>
+    /// Удаление сотрудника
+    /// </summary>
+    /// <param name="id">id сотрудника</param>
+    /// <returns></returns>
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteEmployee(int id)
     {
@@ -21,20 +33,36 @@ public sealed class EmployeeController(EmployeeRepository employeeRepository) : 
         return NoContent();
     }
 
-    [HttpGet("company/{companyId}")]
+    /// <summary>
+    /// Получить список всех доступных полей сотрудников для указанной компании
+    /// </summary>
+    /// <param name="companyId">Id компании</param>
+    /// <returns>Список сотрудников</returns>
+    [HttpGet("{companyId}")]
     public async Task<IActionResult> GetEmployeesByCompanyId(int companyId)
     {
         var employees = await employeeRepository.GetEmployeesByCompanyId(companyId);
         return Ok(employees);
     }
 
-    [HttpGet("department/{departmentName}")]
+    /// <summary>
+    /// Получить список всех доступных полей сотрудников для указного отдела
+    /// </summary>
+    /// <param name="departmentName">Название отдела</param>
+    /// <returns>Список сотрудников</returns>
+    [HttpGet("{departmentName}")]
     public async Task<IActionResult> GetEmployeesByDepartmentName(string departmentName)
     {
         var employees = await employeeRepository.GetEmployeesByDepartmentName(departmentName);
         return Ok(employees);
     }
 
+    /// <summary>
+    /// Изменение сотрудника по его Id. Изменения должно быть только тех полей, которые указаны в запросе.
+    /// </summary>
+    /// <param name="id">id сотрудника</param>
+    /// <param name="employee">Обновленные данные</param>
+    /// <returns></returns>
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateEmployee(int id, [FromBody] Employee employee)
     {
